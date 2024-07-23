1c2728937bc755ebe6677a1a565a60691799d0b0
LeoFernanndes <balbino.leonel@gmail.com>
2024-07-23 01:13:44 -0300

    Adds refresh tokens

--- application/authentication/services/authentication_service.py
+++ application/authentication/services/authentication_service.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 
+from application.authentication.dtos.authentication_dtos import TokenData, TokenPairResponseDto
 from domain.authentication.repositories.user_repository import UserRepository
 
 
@@ -14,18 +15,52 @@ class AuthenticationService:
     def __init__(self, user_repository: UserRepository):
         self._user_repository = user_repository
 
-    # to get a string like this run:
-    # openssl rand -hex 32
     SECRET_KEY = os.getenv("PASSWORD_HASHING_SECRET_KEY")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    REFRESH_TOKEN_EXPIRE_MINUTES = 360
 
-    def create_access_token(self, data: dict, expire_delta: timedelta):
-        to_encode = data.copy()
-        if expire_delta:
-            expire = datetime.now(timezone.utc) + expire_delta
-        else:
-            expire = datetime.now(timedelta.utc) + timedelta(minutes=15)
-        to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
+    def create_token_pair(self, token_data: TokenData) -> TokenPairResponseDto:
+        access_token = self._create_access_token(token_data)
+        refresh_token = self._create_refresh_token(token_data)
+        return TokenPairResponseDto(
+            access_token=access_token,
+            token_duration_minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES,
+            refresh_token=refresh_token,
+            refresh_token_duration_minutes=self.REFRESH_TOKEN_EXPIRE_MINUTES,
+            token_type="bearer"
+        )
+
+    def decode_access_token(self, access_token: str) -> TokenData:
+        payload = jwt.decode(access_token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise Exception("Username not contained in token.")
+        user = self._user_repository.get_by_username(username)
+        if not user:
+            raise Exception("User not found.")
+        return TokenData(username=username, email=user.email)
+
+    def decode_refresh_token(self, refresh_token: str) -> TokenData:
+        payload = jwt.decode(refresh_token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise Exception("Username not contained in token.")
+        user = self._user_repository.get_by_username(username)
+        if not user:
+            raise Exception("User not found.")
+        return TokenData(username=username, email=user.email)
+
+    def _create_access_token(self, token_data: TokenData) -> str:
+        token_data_to_be_encoded = token_data.model_dump()
+        expire = datetime.now(timezone.utc) + timedelta(minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES)
+        token_data_to_be_encoded.update({"exp": expire})  # required format to be used from jwt library
+        encoded_jwt = jwt.encode(token_data_to_be_encoded, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        return encoded_jwt
+
+    def _create_refresh_token(self, token_data: TokenData) -> str:
+        token_data_to_be_encoded = token_data.model_dump()
+        expire = datetime.now(timezone.utc) + timedelta(minutes=self.REFRESH_TOKEN_EXPIRE_MINUTES)
+        token_data_to_be_encoded.update({"exp": expire})  # required format to be used from jwt library
+        encoded_jwt = jwt.encode(token_data_to_be_encoded, self.SECRET_KEY, algorithm=self.ALGORITHM)
         return encoded_jwt
--- presentation/http/fastapi/routers/auth.py
+++ presentation/http/fastapi/routers/auth.py
@@ -1,14 +1,14 @@
 import jwt
 
 from datetime import timedelta
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Header
 from fastapi.exceptions import HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import InvalidTokenError
 from typing import Annotated
 
 from application.authentication.services.authentication_service import AuthenticationService
-from application.authentication.dtos.authentication_dtos import Token, TokenData
+from application.authentication.dtos.authentication_dtos import TokenPairResponseDto, TokenData
 from application.authentication.services.user_service import UserService
 from domain.authentication.entities.user import User
 from presentation.dependencies import get_authentication_service, get_user_service
@@ -19,27 +19,21 @@ auth_router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"}
+)
+
 
 def get_current_user(
         token: Annotated[str, Depends(oauth2_scheme)],
-        user_service: Annotated[UserService, Depends(get_user_service)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
+        auth_service: Annotated[AuthenticationService, Depends(get_authentication_service)]
+    ):
     try:
-        payload = jwt.decode(token, user_service.SECRET_KEY, algorithms=[user_service.ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except InvalidTokenError:
+        auth_service.decode_access_token(access_token=token)
+    except:
         raise credentials_exception
-    user = user_service.get_user_by_username(username)
-    if not user:
-        raise credentials_exception
-    return user
 
 
 @auth_router.post("/token")
@@ -47,19 +41,23 @@ def login_for_access_token(
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
         user_service: Annotated[UserService, Depends(get_user_service)],
         auth_service: Annotated[AuthenticationService, Depends(get_authentication_service)]
-        ) -> Token:
-    user = user_service.authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username of password",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    access_token_expires = timedelta(minutes=user_service.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = auth_service.create_access_token(
-        data={"sub": user.username}, expire_delta=access_token_expires
-    )
-    return Token(access_token=access_token, token_type="bearer")
+        ) -> TokenPairResponseDto:
+    if not user_service.check_password_is_valid(form_data.username, form_data.password):
+        raise credentials_exception
+    user = user_service.get_user_by_username(form_data.username)
+    return auth_service.create_token_pair(TokenData(username=user.username, email=user.email))
+
+
+@auth_router.post("/refresh")
+def refresh_auth_and_refresh_tokens(
+        refresh: Annotated[str, Header()],
+        auth_service: Annotated[AuthenticationService, Depends(get_authentication_service)]
+        ) -> TokenPairResponseDto:
+    try:
+        token_data = auth_service.decode_refresh_token(refresh)
+    except:
+        raise credentials_exception
+    return auth_service.create_token_pair(token_data)
 
 
 @auth_router.get("/me")
